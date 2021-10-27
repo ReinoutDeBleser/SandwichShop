6578b703241d06054b599c0be1768db82b86804d
Reinout <reinout.de.bleser@gmail.com>
2021-10-27 15:34:32 +0200

    working hard on creating switch instead of ifs within validation, using foreach

--- index.php
+++ index.php
@@ -31,18 +31,26 @@ $streetnumberErrInv = '<div class="alert alert-primary" role="alert"> Invalid st
 $zipcodeErrReq = '<div class="alert alert-primary" role="alert"> Zipcode is required </div>';
 $zipcodeErrInv = '<div class="alert alert-primary" role="alert">Invalid zipcode format </div>';
 
-$errors = [
+$errorsReq = [
     ['name' => 'emailErrReq', 'value' => $emailErrReq],
-    ['name' => 'emailErrInv', 'value' => $emailErrInv],
+
     ['name' => 'streetErrReq', 'value' => $streetErrReq],
-    ['name' => 'streetErrInv', 'value' => $streetErrInv],
+
     ['name' => 'cityErrReq', 'value' => $cityErrReq],
-    ['name' => 'cityErrInv', 'value' => $cityErrInv],
+
     ['name' => 'streetnumberErrReq', 'value' => $streetnumberErrReq],
-    ['name' => 'streetnumberErrInv', 'value' => $streetnumberErrInv],
+
     ['name' => 'zipcodeErrReq', 'value' => $zipcodeErrReq],
-    ['name' => 'zipcodeErrInv', 'value' => $zipcodeErrInv]
+
     ];
+$errorsInt = [
+    ['name' => 'emailErrInv', 'value' => $emailErrInv],
+    ['name' => 'streetErrInv', 'value' => $streetErrInv],
+    ['name' => 'cityErrInv', 'value' => $cityErrInv],
+    ['name' => 'streetnumberErrInv', 'value' => $streetnumberErrInv],
+    ['name' => 'zipcodeErrInv', 'value' => $zipcodeErrInv]
+];
+$array = [];
 
 if (isset($_GET["food"]) && $_GET["food"] === "0") {
     $products = [
@@ -60,6 +68,7 @@ if (isset($_GET["food"]) && $_GET["food"] === "0") {
         ['name' => 'Club Salmon', 'price' => 5]
     ];
 }
+//flexible variables created.
 $variables = ["email", "street", "city", "streetnumber", "zipcode"];
 
 foreach ($variables as $v)
@@ -67,99 +76,135 @@ foreach ($variables as $v)
         $$v = $_SESSION[$v];
     }
 
-//backup if foreach would fail.
-//if (isset($_SESSION["street"]) && !empty($_SESSION["street"])) {
-//    $street = $_SESSION["street"];
-//}
-//if (isset($_SESSION["city"]) && !empty($_SESSION["city"])) {
-//    $city = $_SESSION["city"];
-//}
-//if (isset($_SESSION["streetnumber"]) && !empty($_SESSION["streetnumber"])) {
-//    $streetnumber = $_SESSION["streetnumber"];
-//}
-//if (isset($_SESSION["zipcode"]) && !empty($_SESSION["zipcode"])) {
-//    $zipcode = $_SESSION["zipcode"];
-//}
-
 if (isset($_SESSION["totalValue"]) && !empty($_SESSION["totalValue"])) {
     $totalValue = $_SESSION["totalValue"];
 } else {
     $totalValue = 0;
 }
-
+// creative constraint: can't echo errors in index, has to happen in form-view
+// figure out how to make it so that if no errors or all fields valid that email gets sent & total value calc'd
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //Validation for email
-    if (empty($_POST["email"])) {
-        //will throw REQUIRED error if empty
-        echo  $emailErrReq;
-    } else {
-        $email = $_POST["email"];
-        $_SESSION["email"] = $email;
-        // check if e-mail address is well-formed
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            //will throw INVALID error if invalid format (or characters?) are used.
-            echo $emailErrInv;
+    foreach ($variables as $w){
+        //echo $w;
+        if (empty($_POST[$w])) {
+            //will throw REQUIRED error if empty
+            //how should this be implemented? not really clear to me
+            $var = $$w;
+            "$".$var."ErrReq";
+            echo $wat;
+        } else {
+            $$w = $_POST[$w];
+            $_SESSION[$w] = $$w;
+            // check if e-mail address is well-formed
+            switch ($w) {
+                case "email":
+                {if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                        //will throw INVALID error if invalid format (or characters?) are used.
+                    echo $emailErrInv;
+                    $array[] = 'emailErrInv';
+                    break;
+                }}
+                case "street":
+                {if (!preg_match('/^[\p{L} ]+$/u', $street)) {
+                    echo $streetErrInv;
+                    $array[] = 'streetErrInv';
+                    break;
+                }}
+                case ("city"):
+                {if (!preg_match('/^[\p{L} ]+$/u', $city)) {
+                    echo $cityErrInv;
+                    $array[] = 'cityErrInv';
+                    break;
+                }}
+                case "streetnumber":
+                {  if (!filter_var($streetnumber, FILTER_SANITIZE_NUMBER_INT)) {
+                    echo $streetnumberErrInv;
+                    $array[] = 'streetnumberErrInv';
+                    break;
+                }}
+                case "zipcode":
+                {if (!filter_var($zipcode, FILTER_SANITIZE_NUMBER_INT)) {
+                    echo $zipcodeErrInv;//create an element in the HTML you can't do that with the cookie.
+                    $array[] = 'zipcodeErrInv';
+                    break;
+                }}
         }
-    }
-    if (empty($_POST["street"])) {
-        echo $streetErrReq;
-    } else {
-        $street = $_POST["street"];
-        $_SESSION["street"] = $street;
-        // check if e-mail address is well-formed
-        if (!preg_match('/^[\p{L} ]+$/u', $street)) {
-            echo $streetErrInv;
-        }
-    }
-    if (empty($_POST["city"])) {
-        echo $cityErrReq;
-    } else {
-        $city = $_POST["city"];
-        $_SESSION["city"] = $city;
-        // check if city address is well-formed
-        if (!preg_match('/^[\p{L} ]+$/u', $city)) {
-            echo $cityErrInv;
-        }
-    }
-    if (empty($_POST["streetnumber"])) {
-        echo $streetnumberErrReq;
+                //will throw INVALID error if invalid format (or characters?) are used.
 
-    } else {
-        $streetnumber = $_POST["streetnumber"];
-        $_SESSION["streetnumber"] = $streetnumber;
-        // check if e-mail address is well-formed
-        if (!filter_var($streetnumber, FILTER_SANITIZE_NUMBER_INT)) {
-            echo $streetnumberErrInv;
-        }
     }
-    if (empty($_POST["zipcode"])) {
-        echo $zipcodeErrReq;
-    } else {
-        $zipcode = $_POST["zipcode"];
-        $_SESSION["zipcode"] = $zipcode;
-        // check if e-mail address is well-formed
-        if (!filter_var($zipcode, FILTER_SANITIZE_NUMBER_INT)) {
-            echo $zipcodeErrInv;
-        }
     }
+//    if (empty($_POST["email"])) {
+//        //will throw REQUIRED error if empty
+//        //how should this be implemented? not really clear to me
+//        echo $emailErrReq;
+//    } else {
+//        $email = $_POST["email"];
+//        $_SESSION["email"] = $email;
+//        // check if e-mail address is well-formed
+//        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+//            //will throw INVALID error if invalid format (or characters?) are used.
+//            echo $emailErrInv;
+//        }
+//    }
+//    if (empty($_POST["street"])) {
+//        echo $streetErrReq;
+//    } else {
+//        $street = $_POST["street"];
+//        $_SESSION["street"] = $street;
+//        // check if e-mail address is well-formed
+//        if (!preg_match('/^[\p{L} ]+$/u', $street)) {
+//            echo $streetErrInv;
+//        }
+//    }
+//    if (empty($_POST["city"])) {
+//        echo $cityErrReq;
+//        $array[] = 'cityErrReq';
+//    } else {
+//        $city = $_POST["city"];
+//        $_SESSION["city"] = $city;
+//        // check if city address is well-formed
+//        if (!preg_match('/^[\p{L} ]+$/u', $city)) {
+//            echo $cityErrInv;
+//            $array[] = 'cityErrInv';
+//        }
+//    }
+//    if (empty($_POST["streetnumber"])) {
+//        echo $streetnumberErrReq;
+//        $array[] = 'streetnumberErrReq';
+//
+//    } else {
+//        $streetnumber = $_POST["streetnumber"];
+//        $_SESSION["streetnumber"] = $streetnumber;
+//        // check if e-mail address is well-formed
+//        if (!filter_var($streetnumber, FILTER_SANITIZE_NUMBER_INT)) {
+//            echo $streetnumberErrInv;
+//            $array[] = 'streetnumberErrInv';
+//        }
+//    }
+//    if (empty($_POST["zipcode"])) {
+//        echo $zipcodeErrReq;
+//        $array[] = "zipcodeErrReq";
+//    } else {
+//        $zipcode = $_POST["zipcode"];
+//        $_SESSION["zipcode"] = $zipcode;
+//        // check if e-mail address is well-formed
+//        if (!filter_var($zipcode, FILTER_SANITIZE_NUMBER_INT)) {
+//            echo $zipcodeErrInv;//create an element in the HTML you can't do that with the cookie.
+//            $array[] = 'zipcodeErrInv';
+//        }
+//    }
     //Todo: make the requirements work.
-    if (
-        ($emailErrReq === "" && $emailErrInv === "") &&
-        ($streetErrReq === ""&& $emailErrInv === "") &&
-        ($cityErrReq === "" && $cityErrInv === "") &&
-        ($streetnumberErrReq === "" && $streetnumberErrInv === "") &&
-        ($zipcodeErrReq === "" && $zipcodeErrInv === "")
-    ) {
+// create an array, then add values.
+    if (count($array) == 0){
         //Todo: object structuring and destructuring DONE.
         //with foreach loop using the structure of the checkbox generator to cycle through all checked values
         //and returning a 0 value if unchecked so that they are still able to be added to the $_SESSION;
         foreach ($products as $i => $product) {
             if (isset($_POST["products"][$i])) {
-                $addedName[$i] = $product['name'];
-                //echo $addedName[$i];
-                print_r($addedName[$i]);
+
+                $addedName[$i] = [$product['name'], $product['price']];
                 $addedValue[$i] = $product['price'];
-                echo $addedValue[$i];
                 $totalValue += $product['price'];
                 $_SESSION['totalValue'] = $totalValue;
             }
@@ -173,32 +218,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             echo $valid = '<div class="alert alert-primary" role="alert"> Your order has been sent. ETA: 2 hours </div>';
         }
         //Todo: email  used to work in echo format.
-//        echo "Hi there $email,<br>
-//              We will send your order to $street $streetnumber, $city, $zipcode,<br>
-//              Thanks for your business dear customer,<br>
-//              The items you ordered were:<br>";
-//        foreach ($products as $i => $product) {
-//            if (isset($_POST["products"][$i])) {
-//                echo $addedName[$i] . " cost you " . $addedValue[$i] . "€ <br>";
-//            }
-//        }
+        echo "Hi there $email,<br>
+              We will send your order to $street $streetnumber, $city, $zipcode,<br>
+              Thanks for your business dear customer,<br>
+              The items you ordered were:<br>";
+        foreach ($products as $i => $product) {
+            if (isset($_POST["products"][$i])) {
+                echo $addedName[$i] . " cost you " . $addedValue[$i] . "€ <br>";
+            }
+        }
 //array creation for clicked boxes to order from. validation of those elements and the sending of the email with the correct information.
         if (isset($_POST['express_delivery'])) {
             echo "Express delivery cost you $express € <br>";
         }
-//                    foreach ($products as $i => $product) {
-//                    if (isset($_POST["products"][$i])) {
-//                    echo $addedName[$i] . " cost you " . $addedValue[$i] . "€ \n";
-//                      }
-//                     }
         $receiver = "reinout.de.bleser@gmail.com";
         $subject = "Purchase overview";
         $body = "Hi there $email,\n
                 We will send your order to $street $streetnumber, $city, $zipcode,\n
-                    Thanks for your business dear customer,\n
-                    The items you ordered were:\n
+                Thanks for your business dear customer,\n
+                The items you ordered were:\n".
 
-                    The total cost of your order was $totalValue €";
+                "The total cost of your order was $totalValue €";
+
+
         $sender = "From:reinout.de.bleser@gmail.com";
         if (mail($receiver, $subject, $body, $sender)) {
             echo "Email sent successfully to $receiver";
@@ -209,11 +251,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo $valid = "";
     }
 }
-
-
-
-
-
 
 whatIsHappening();
 
